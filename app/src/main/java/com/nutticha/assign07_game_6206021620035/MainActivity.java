6048479fd8c42787eb492e1af34e4445fb9256db
Nutticha <nutticha30234@gmail.com>
2022-04-21 21:52:42 +0700

    Initial commit

---
+++ app/src/main/java/com/nutticha/assign07_game_6206021620035/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nutticha.assign07_game_6206021620035;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
